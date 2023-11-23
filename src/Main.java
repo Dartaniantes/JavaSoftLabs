c8930d1ef608bcd4dcab6a23e55da64d7312894b
Dartaniantes <demkacorporation@gmail.com>
2023-11-23 15:50:04 +0300

    Lab distribution comments added in Main.main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // Lab 1
         //Counter.count();
+
+        // Lab 2
         Matrix.XorAverages();
+
+        // Lab 3
         //WordSorter.sort();
 
 
